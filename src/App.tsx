58a455b0e106807e47e887eddcec8a7dfa310e49
Crow-yoo <dbwltkd5984@gmail.com>
2024-10-29 14:41:04 +0900

    #1 React 실습

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+{/*
+  작성자 : yoo
+  작성일 : 29.10.2024
+  내용 : 기능에 대한 내용 및 실습  
+*/}
+
 function App() {
+  let name = "React";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='test'>Hello,
+        {
+        name === 'React' ? (<h1>YES</h1>): null
+        }!!</h1>
+      <p>반갑습니다.</p>
     </div>
   );
 }
